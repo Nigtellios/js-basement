30937430cfdac883d51cf29749c1e5afdd3cc75c
Jakubinka <nigtellios@gmail.com>
2020-11-05 11:37:48 +0100

    v0.02 - Some Bug
    Well, idk what is going on with VSC or my fake config. xD

--- js/script.js
+++ js/script.js
@@ -1,38 +1,19 @@
-/* Task 7 - Arrays */
+/* Task 8 - Multidimensional Arrays */
 
-var task7 = "#Task 7: ";
+var task8 = "#Task 8: ";
 
-let inventory = ["shirt", "axe", "sword"];
-//inventory = ["water", "pants"]; We can ofc change the values if we want to something different.
+let levels = [
+  [1.1, 1.2, 1.3, 1.4], //Array 1
+  [2.1, 2.2], //Array 2
+  [3.1, 3.2, 3.3], //Array 3
+  [4.1, 4.2, 4.3, 4.4, 4.5] //Array 4
+];
 
-//Let's pretend that we know what is in the array and we want something from it.
+//^Actually we created a 4 arrays inside array, that is called multidimensional arrays.
+//We wanna simulate few levels of our game while every of them has some sub-levels inside.
+//We can assume that they are like rows and columns. In each of inner-array we have some 
+//columns, these columns are: 1.1, 1.2, 1.3, 1.4 etc.
 
-let shirt = inventory[0];
+console.log(task8 + "1" + levels[1]);
 
-//But we can access and modify existing element. 
-
-inventory[2] = "bread";
-
-//We can return a whole length of an array and simply a length of single position in an array.
-
-let howLong = inventory.length; // It would return 3.
-
-//Now let's get length of item with index 2 (so 3rd in a row).
-
-let howLongItem = inventory[2].length;
-
-//We can push item to the end of the array via push() method.
-
-let pushArray = inventory.push("potion");
-
-//We can also delete an item from array using pop() method.
-
-let popArray = inventory.pop();
-
-console.log(task7 + inventory[2]);
-console.log(task7 + "Our array has: " + howLong + " positions.");
-console.log(task7 + "Item with index 2 is: " + howLongItem + " letters long.");
-console.log(task7 + "Our whole array after push: " + inventory);
-console.log(task7 + "Our whole array after pop: " + inventory);
-
-/* End of Task 7 --------- */
+/* End of Task 8 --------- */
