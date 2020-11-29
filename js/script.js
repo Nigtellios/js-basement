15f58d7a9c995a7a0f5a10657a6ef5ac7a79a2f3
Jakubinka <nigtellios@gmail.com>
2020-11-29 22:20:18 +0100

    v0.04 - Task 16

--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@
 
 var task15 = "#Task 15: ";
 
+let endOfMap = 500;
+let aPos = 0;
+let enemyPos = 450;
+let isGameOver = false;
 
+while() {
+  
+}
 
-console.log(task15 + "Our actual position: " + aPos);
+console.log(task15 + " ");
 
 /* End of Task 15 --------- */
