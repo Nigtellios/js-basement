38242d76b18f99b6188cdafe7d01433057221f9b
Jakubinka <nigtellios@gmail.com>
2020-11-29 20:28:25 +0100

    v0.04 - Task 15

--- js/script.js
+++ js/script.js
@@ -1,23 +1,9 @@
-/* Task 11 - Function Return Statements */ 
+/* Task 15 - Control Statements */ 
 
-var task11 = "#Task 11: ";
+var task15 = "#Task 15: ";
 
-let maxBalance = 200.0;
-let currentBalance = 198.1;
 
-//Now we want to calculate a precentage of our balance, during this we would 
-//learn how to return something from a function to make some calculations.
 
-function transaction(transValue = 1.0) {
-  let calcValue = currentBalance + transValue;
-  currentBalance = calcValue > 200 ? maxBalance : calcValue;
-  currentBalance -= transValue;
-  return (currentBalance / maxBalance) * 100; //Calculating the precentage.
-}
+console.log(task15 + "Our actual position: " + aPos);
 
-let result = transaction();
-result = transaction(20.9);
-
-console.log(task11 + "Current Balance after transaction: " + currentBalance);
-
-/* End of Task 11 --------- */
+/* End of Task 15 --------- */
