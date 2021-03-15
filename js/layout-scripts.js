f6ec8be1ec2ad9fd4196e62bb7114b8c58919ab8
Jakub Kowalski <shadowinkowal@gmail.com>
2021-03-15 11:56:37 +0100

    🐱‍👤Added: Zenva Toolbar functionality

--- js/layout-scripts.js
+++ js/layout-scripts.js
@@ -1,31 +1,60 @@
 /* Assign FreeCodeCamp Toolbar buttons */
 const bttFccTop = document.getElementById('go-to-top-fcc');
 const bttFccBot = document.getElementById('go-to-bot-fcc');
+const bttZTop = document.getElementById('go-to-top-zenva');
+const bttZBot = document.getElementById('go-to-bot-zenva');
 
 /* Assign FreeCodeCamp and Zenva slider divs */
 let fcc = document.getElementById('fcc');
 let zenva = document.getElementById('zenva');
+let myOwn = document.getElementById('my-own');
 
-function scrollToTop() {
+
+/* FCC */
+function FCCscrollToTop() {
   fcc.scrollTo({
     top: 0,
     behavior: 'smooth'
   });
 }
 
-function scrollToBot() {
-  /* Reverse Y Axis, scrollHeight instead of offsetTop + offsetHeight... There was also scrollIntoView (but scollIntoView works for whole page) */
+function FCCscrollToBot() {
   fcc.scrollTo({
     top: fcc.scrollHeight,
     behavior: 'smooth'
   });
 }
 
+/* Zenva */
+function ZscrollToTop() {
+  zenva.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+function ZscrollToBot() {
+  zenva.scrollTo({
+    top: zenva.scrollHeight,
+    behavior: 'smooth'
+  });
+}
+
+/* My Own */
+
 /* Check if buttons exists before listening to it */
 if (bttFccTop !== null) {
-  bttFccTop.addEventListener('click', scrollToTop);
+  bttFccTop.addEventListener('click', FCCscrollToTop);
 } 
 
 if (bttFccBot !== null) {
-  bttFccBot.addEventListener('click', scrollToBot);
+  bttFccBot.addEventListener('click', FCCscrollToBot);
+}
+
+if (bttZTop !== null) {
+  bttZTop.addEventListener('click', ZscrollToTop);
+} 
+
+if (bttZBot !== null) {
+  bttZBot.addEventListener('click', ZscrollToBot);
 }
