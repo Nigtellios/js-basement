6ef738a2c71c0b25824ace553f4551b8ef07c1c7
Jakub Kowalski <shadowinkowal@gmail.com>
2021-04-01 19:10:51 +0200

    🐱‍👤Finished: FCC REGEX Task 3

--- js/tasks/fcc-js-regex.js
+++ js/tasks/fcc-js-regex.js
@@ -55,3 +55,28 @@ if (btnFCCC2 !== null && scopeFCCC2 !== null) {
 } else {
   scopeFCCC2.innerHTML = "ERROR: Button ID or box scope ID doesn't exist or has the wrong value.";
 }
+
+/* Task C3: Match a Literal String with Different Possibilities */
+
+/* Using regexes like /coding/, you can look for the pattern coding in another string. This is powerful to search single strings, but it's limited to only one pattern. You can search for multiple patterns using the alternation or OR operator: |. */
+
+/* This operator matches patterns either before or after it. For example, if you wanted to match the strings yes or no, the regex you want is /yes|no/. You can also search for more than just two patterns. You can do this by adding more patterns with more OR operators separating them, like: /yes|no|maybe/. */
+
+let vodkaPattern = "Where is my vOdka, Jurij?";
+let testVodkaPattern = /vodka|Vodka|VODKA|vOdka|vODKA/;
+let vodkaResult = testVodkaPattern.test(vodkaPattern);
+
+/* OUTPUT CODE */
+
+const btnFCCC3 = document.getElementById('run-fcc-task-c3');
+const scopeFCCC3 = document.getElementById('fcc-task-c3');
+
+function debugFCCC3() {
+  scopeFCCC3.innerHTML = `${outputResponse} Is there any vodka? ${vodkaResult}`;
+}
+
+if (btnFCCC3 !== null && scopeFCCC3 !== null) {
+  btnFCCC3.addEventListener('click', debugFCCC3);
+} else {
+  scopeFCCC3.innerHTML = "ERROR: Button ID or box scope ID doesn't exist or has the wrong value.";
+}
