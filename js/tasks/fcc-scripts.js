0701a43347c9ecd8892a3aea75c1cb4884c9e496
Jakub Kowalski <shadowinkowal@gmail.com>
2021-04-01 13:33:57 +0200

    🐱‍👤Finished: FCC ES6 Tasks 24-26

--- js/tasks/fcc-scripts.js
+++ js/tasks/fcc-scripts.js
@@ -3867,11 +3867,96 @@ const btnFCCB23= document.getElementById('run-fcc-task-b23');
 const scopeFCCB23 = document.getElementById('fcc-task-b23');
 
 function debugFCCB23() {
-  scopeFCCB23.innerHTML = `${outputResponse} ${add2(14,16)}`;
+  scopeFCCB23.innerHTML = `${outputResponse} ${add2(14,16)}, and there is no more coverage of this subject.`;
 }
 
 if (btnFCCB23 !== null && scopeFCCB23 !== null) {
   btnFCCB23.addEventListener('click', debugFCCB23);
 } else {
   scopeFCCB23.innerHTML = "ERROR: Button ID or box scope ID doesn't exist or has the wrong value.";
+}
+
+/* Task B24: Reuse JavaScript Code Using import */
+
+/* import allows you to choose which parts of a file or module to load. In the previous lesson, the examples exported add from the math_functions.js file. Here's how you can import it to use in another file: */
+
+// import { add } from './math_functions.js';
+
+/* Here, import will find add in math_functions.js, import just that function for you to use, and ignore the rest. the ./ tells the import to look for the math_functions.js file in the same folder as the current file. The relative file path (./) and file extension (.js) are required when using import in this way. You can import more than one item from the file by adding them in the import statement like this: */
+
+// import { add, subtract } from './math_functions.js';
+
+/* OUTPUT CODE */
+
+const btnFCCB24= document.getElementById('run-fcc-task-b24');
+const scopeFCCB24 = document.getElementById('fcc-task-b24');
+
+function debugFCCB24() {
+  scopeFCCB24.innerHTML = `${outputResponse} There is no more coverage of this subject.`;
+}
+
+if (btnFCCB24 !== null && scopeFCCB24 !== null) {
+  btnFCCB24.addEventListener('click', debugFCCB24);
+} else {
+  scopeFCCB24.innerHTML = "ERROR: Button ID or box scope ID doesn't exist or has the wrong value.";
+}
+
+/* Task B25: Use * to Import Everything from a File */
+
+/* Suppose you have a file and you wish to import all of its contents into the current file. This can be done with the import * as syntax. Here's an example where the contents of a file named math_functions.js are imported into a file in the same directory: */
+
+// import * as myMathModule from "./math_functions.js";
+
+/* The above import statement will create an object called myMathModule. This is just a variable name, you can name it anything. The object will contain all of the exports from math_functions.js in it, so you can access the functions like you would any other object property. Here's how you can use the add and subtract functions that were imported: */
+
+// myMathModule.add(2,3);
+// myMathModule.subtract(5,3);
+
+/* OUTPUT CODE */
+
+const btnFCCB25= document.getElementById('run-fcc-task-b25');
+const scopeFCCB25 = document.getElementById('fcc-task-b25');
+
+function debugFCCB25() {
+  scopeFCCB25.innerHTML = `${outputResponse} There is no more coverage of this subject.`;
+}
+
+if (btnFCCB25!== null && scopeFCCB25 !== null) {
+  btnFCCB25.addEventListener('click', debugFCCB25);
+} else {
+  scopeFCCB25.innerHTML = "ERROR: Button ID or box scope ID doesn't exist or has the wrong value.";
+}
+
+/* Task B26: Create an Export Fallback with export default */
+
+/* In the export lesson, you learned about the syntax referred to as a named export. This allowed you to make multiple functions and variables available for use in other files. There is another export syntax you need to know, known as export default. Usually you will use this syntax if only one value is being exported from a file. It is also used to create a fallback value for a file or module.
+
+Below are examples using export default: */
+
+/*
+ export default function add(x, y) {
+  return x + y;
+ }
+*/
+/*
+ export default function(x, y) {
+  return x + y;
+ }
+*/
+
+/* The first is a named function, and the second is an anonymous function. Since export default is used to declare a fallback value for a module or file, you can only have one value be a default export in each module or file. Additionally, you cannot use export default with var, let, or const. */
+
+/* OUTPUT CODE */
+
+const btnFCCB26= document.getElementById('run-fcc-task-b26');
+const scopeFCCB26 = document.getElementById('fcc-task-b26');
+
+function debugFCCB26() {
+  scopeFCCB26.innerHTML = `${outputResponse} There is no more coverage of this subject.`;
+}
+
+if (btnFCCB26!== null && scopeFCCB26 !== null) {
+  btnFCCB26.addEventListener('click', debugFCCB26);
+} else {
+  scopeFCCB26.innerHTML = "ERROR: Button ID or box scope ID doesn't exist or has the wrong value.";
 }
