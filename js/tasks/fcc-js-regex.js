48535e9f67a5579affbf19a2c726ed6a6eda117c
Jakub Kowalski <shadowinkowal@gmail.com>
2021-04-06 13:32:30 +0200

    🐱‍👤Finished: FCC REGEX Task 12

--- js/tasks/fcc-js-regex.js
+++ js/tasks/fcc-js-regex.js
@@ -287,3 +287,27 @@ if (btnFCCC11 !== null && scopeFCCC11 !== null) {
 } else {
   scopeFCCC11.innerHTML = "ERROR: Button ID or box scope ID doesn't exist or has the wrong value.";
 }
+
+/* Task C12: Match Characters that Occur One or More Times */
+
+/* Sometimes, you need to match a character (or group of characters) that appears one or more times in a row. This means it occurs at least once, and may be repeated. You can use the + character to check if that is the case. Remember, the character or pattern has to be present consecutively. That is, the character has to repeat one after the other. */
+
+/* For example, /a+/g would find one match in abc and return ["a"]. Because of the +, it would also find a single match in aabc and return ["aa"]. If it were instead checking the string abab, it would find two matches and return ["a", "a"] because the a characters are not in a row - there is a b between them. Finally, since there is no a in the string bcd, it wouldn't find a match. */
+
+let twelfthStr = "Missouri";
+let testTwelfthRegex = /s+/gi;
+
+/* OUTPUT CODE */
+
+const btnFCCC12 = document.getElementById('run-fcc-task-c12');
+const scopeFCCC12 = document.getElementById('fcc-task-c12');
+
+function debugFCCC12() {
+  scopeFCCC12.innerHTML = `${outputResponse} Our match: ${twelfthStr.match(testTwelfthRegex)}.`;
+}
+
+if (btnFCCC12 !== null && scopeFCCC12 !== null) {
+  btnFCCC12.addEventListener('click', debugFCCC12);
+} else {
+  scopeFCCC12.innerHTML = "ERROR: Button ID or box scope ID doesn't exist or has the wrong value.";
+}
