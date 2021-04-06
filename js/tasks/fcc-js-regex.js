a9a85c3abcb5e6715669960b076f562d2df86aad
Jakub Kowalski <shadowinkowal@gmail.com>
2021-04-06 10:22:06 +0200

    🐱‍👤Fixed: FCC REGEX Task 7

--- js/tasks/fcc-js-regex.js
+++ js/tasks/fcc-js-regex.js
@@ -87,7 +87,7 @@ if (btnFCCC3 !== null && scopeFCCC3 !== null) {
 
 /* Case (or sometimes letter case) is the difference between uppercase letters and lowercase letters. Examples of uppercase are A, B, and C. Examples of lowercase are a, b, and c. */
 
-/* You can match both cases using what is calles a flag. There are other flags but here you'll focus on the flag that ignores case - the i flag. You can use it by appending it to the regex. An example of using this flag is /ignorecase/i. This regex can match the strings ignorecase, igNoreCase, and IgnoreCase. */
+/* You can match both cases using what is called a flag. There are other flags but here you'll focus on the flag that ignores case - the i flag. You can use it by appending it to the regex. An example of using this flag is /ignorecase/i. This regex can match the strings ignorecase, igNoreCase, and IgnoreCase. */
 
 let fourthRegex = "WhAt are we doing?";
 let testFourthRegex = /what/i;
@@ -108,17 +108,16 @@ if (btnFCCC4 !== null && scopeFCCC4 !== null) {
   scopeFCCC4.innerHTML = "ERROR: Button ID or box scope ID doesn't exist or has the wrong value.";
 }
 
-/* Task C5: Extract Matches - TO CHECK */
+/* Task C5: Extract Matches */
 
 /* So far, you have only been checking if a pattern exists or not within a string. You can also extract the actual matches you found with the .match() method. */
 
 /* To use the .match() method, apply the method on a string and pass in the regex inside the parentheses. Here is the example: */
-  n 
 "This request has been denIed.".match(/denied/i);
 let fifthRegex = "Where is the David?";
 let testFifthRegex = /david/i;
 
-/* Here the first matdh would return ["denied"] and the second would return ["david"]. Note that the .match syntax is the "opposite" of the .test method you have been using this far. */
+/* Here the first match would return ["denied"] and the second would return ["david"]. Note that the .match syntax is the "opposite" of the .test method you have been using this far. */
 
 // 'string'.match(/regex/);
 // /regex/.test('string');
@@ -161,15 +160,15 @@ if (btnFCCC6 !== null && scopeFCCC6 !== null) {
   scopeFCCC6.innerHTML = "ERROR: Button ID or box scope ID doesn't exist or has the wrong value.";
 }
 
-/* Task C7: Match Anything with Wildcard Peroid */
+/* Task C7: Match Anything with Wildcard Period */
 
 /* Sometimes you won't (or don't need to) know the exact characters in your patterns. Thinking of all words that match, say, a misspelling would take a long time. Luckily, you can save time using the wildcard character: . */
 
-/* The wildcard character . will match any one character. The wildcard is also called dot and peroid. You can use the wildcard character just like any other character in the regex. For example, if you wanted to match hug, huh, hut, and hum, you can use the refex /hu./ to match all four words. */
+/* The wildcard character . will match any one character (1 length). The wildcard is also called dot and period. You can use the wildcard character just like any other character in the regex. For example, if you wanted to match hug, huh, hut, and hum, you can use the regex /hu./ to match all four words. */
 
-let seventhRegex1 = "Camera is on!";
-let seventhRegex2 = "Castle has been built.";
-let testSeventhRegex = /ca./;
+let seventhRegex1 = "Cam is on!";
+let seventhRegex2 = "Cas has been built.";
+let testSeventhRegex = /ca./i;
 
 /* OUTPUT CODE */
 
@@ -185,3 +184,4 @@ if (btnFCCC7 !== null && scopeFCCC7 !== null) {
 } else {
   scopeFCCC7.innerHTML = "ERROR: Button ID or box scope ID doesn't exist or has the wrong value.";
 }
+
