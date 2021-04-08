d83924d6a6984dc76ed4389b696a3059270f373b
Jakub Kowalski <shadowinkowal@gmail.com>
2021-04-08 18:36:19 +0200

    🐱‍👤Finished: FCC REGEX Task 29

--- js/tasks/fcc-js-regex.js
+++ js/tasks/fcc-js-regex.js
@@ -738,3 +738,37 @@ if (btnFCCC28 !== null && scopeFCCC28 !== null) {
 } else {
   scopeFCCC28.innerHTML = "ERROR: Button ID or box scope ID doesn't exist or has the wrong value.";
 }
+
+/* Task C29: Positive and Negative Lookahead */
+
+/* Lookaheads are patterns that tell JavaScript to look-ahead in your string to check for patterns further along. This can be useful when you want to search for multiple patterns over the same string. There are two kinds of lookaheads: positive lookahead and negative lookahead. */
+
+/* A positive lookahead will look to make sure the element in the search pattern is there, but won't actually match it. A positive lookahead is used as (?=...) where the ... is the required part that is not matched. On the other hand, a negative lookahead will look to make sure the element in the search pattern is not there. A negative lookahead is used as (?!...) where the ... is the pattern that you do not want to be there. The rest of the pattern is returned if the negative lookahead part is not present. They are a lil-bit confusing, but some examples could help. */
+
+let quit = "qu";
+let noQuit = "qt";
+let quitRegex = /q(?=u)/;
+let noQuitRegex = /q(?!u)/;
+let quitResult = quit.match(quitRegex);
+let noQuitResult = noQuit.match(noQuitRegex);
+
+/* Both of these match calls would return ["q"]. A more practical use of lookaheads is to check two or more patterns in one string. Here is a (naively) simple password checker that looks for between 3 and 6 characters and at least one number: */
+
+let passwordC29 = "abc1337";
+let checkPasswordC29 = /(?=\w{3,6})(?=\D*\d)/;
+let passwordCheckResult = checkPasswordC29.test(passwordC29);
+
+/* OUTPUT CODE */
+
+const btnFCCC29 = document.getElementById('run-fcc-task-c29');
+const scopeFCCC29 = document.getElementById('fcc-task-c29');
+
+function debugFCCC29() {
+  scopeFCCC29.innerHTML = `${outputResponse} Our lookaheads with quits: ${quitResult}, and no quit: ${noQuitResult}. Our lookahead for password that looks for between 3 and 6 characters and at least 1 number: ${passwordCheckResult}.`;
+}
+
+if (btnFCCC29 !== null && scopeFCCC29 !== null) {
+  btnFCCC29.addEventListener('click', debugFCCC29);
+} else {
+  scopeFCCC29.innerHTML = "ERROR: Button ID or box scope ID doesn't exist or has the wrong value.";
+}
